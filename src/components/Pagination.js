3ada4af64fb2c3540be6f9171951b7d1858e9f2e
Jakz Aizzat <jakzaizzat@gmail.com>
2021-02-08 23:12:49 +0800

    📦 NEW: Add Pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Pagination = ({ pageNumber, totalPages, onNextPage, onPreviousPage }) => {
+  return (
+    <div className="flex items-center justify-between py-3">
+      {pageNumber > 1 ? (
+        <button onClick={onPreviousPage}>
+          <svg
+            className="w-4 h-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
+        </button>
+      ) : (
+        <div></div>
+      )}
+      <div>
+        {pageNumber} of {totalPages}
+      </div>
+
+      {pageNumber < 10 ? (
+        <button onClick={onNextPage}>
+          <svg
+            className="w-4 h-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </button>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
+export default Pagination;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from "react";
 import PostList from "../components/PostList";
 import { getPosts } from "../api/posts";
+import Pagination from "../components/Pagination";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const PAGE_SIZE = 10;
+
+  function handleNextPage() {
+    if (pageNumber > 9) return;
+    setPageNumber(pageNumber + 1);
+  }
+
+  function handlePreviousPage() {
+    if (pageNumber < 2) return;
+    setPageNumber(pageNumber - 1);
+  }
+
+  function paginatePosts(posts, pageNumber) {
+    return posts.slice((pageNumber - 1) * PAGE_SIZE, pageNumber * PAGE_SIZE);
+  }
 
   useEffect(() => {
     getPosts().then((response) => setPosts(response));
@@ -12,7 +30,13 @@ const Home = () => {
   return (
     <div className="container">
       <h1 className="font-black text-2xl mb-8">Blog</h1>
-      <PostList posts={posts} />
+      <PostList posts={paginatePosts(posts, pageNumber)} />
+      <Pagination
+        pageNumber={pageNumber}
+        totalPages={PAGE_SIZE}
+        onNextPage={handleNextPage}
+        onPreviousPage={handlePreviousPage}
+      />
     </div>
   );
 };
